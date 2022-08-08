5029801841719253647c0964b53370c489ed6304
eoeefosa <eoeefosa@gmail.com>
2022-08-08 22:27:02 +0100

    modified main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,6 @@
 import 'playexamples/components.dart';
 import 'package:flutter/material.dart';
 
-// This example mirrors the previous one, creating the same list using
-// ListView.builder constructor.
-// Using the IndexedWidgetBuilder,
-// children are built lazily and can be infinite in number
 void main() {
   runApp(ListviewBuilder());
 }
